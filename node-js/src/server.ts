51010127baa718cb5eb9435780706e34aba2b363
weijie <qoo77823@hotmail.com>
2026-04-18 20:37:22 +0800

    chore: server.ts add listen 0.0.0.0 for docker environment

--- node-js/src/server.ts
+++ node-js/src/server.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 async function bootstrap() {
   const app = await createApp();
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0" , () => {
     logger.info(`server.start Node.js server started on port ${PORT}`, {
       service: "web",
       port: PORT,
